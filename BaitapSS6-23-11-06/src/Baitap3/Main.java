f2795ab29d5755b3189c5a5aabd9e5f2de9f9444
VuVietAnh <6sa7an6@gmail.com>
2023-11-08 01:30:24 +0900

    add file

--- BaitapSS6-23-11-06/src/Baitap3/Main.java
+++ BaitapSS6-23-11-06/src/Baitap3/Main.java
@@ -5,10 +5,8 @@ import Baitap3.Point.Point;
 
 public class Main {
     public static void main(String[] args) {
-        Point newPoint = new Point();
-        newPoint.setXY(1,5);
-        MoveablePoint newMoveablePoint = new MoveablePoint();
-        newMoveablePoint.setSpeed(5,8);
+        Point newPoint = new Point(1,5);
+        MoveablePoint newMoveablePoint = new MoveablePoint(1,5,8,9);
         newMoveablePoint.move();
         System.out.println("newPoint" + newPoint);
         System.out.println("newMoveablePoint" + newMoveablePoint);
